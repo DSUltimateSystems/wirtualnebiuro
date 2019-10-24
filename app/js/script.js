74ec392dca23eb67a0de7d9d63b5cc5fcbfd6c75
Dominika Świeboda <dominika_swieboda@outlook.com>
2019-10-24 21:43:41 +0200

    add sort table

--- app/js/script.js
+++ app/js/script.js
@@ -48,3 +48,37 @@ const App = (() => {
 
 // Init
 App.init();
+
+/* List.js is required to make this table work. */
+
+var options = {
+  valueNames: [ { data: ['timestamp'] }, { data: ['status'] }, 'jSortNumber', 'jSortName', 'jSortTotal' ],
+  page: 6,
+  pagination: {
+    innerWindow: 1,
+    left: 0,
+    right: 0,
+    paginationClass: "pagination",
+  }
+};
+
+var tableList = new List('table', options);
+
+$('.jPaginateNext').on('click', function(){
+  var list = $('.pagination').find('li');
+  $.each(list, function(position, element){
+    if($(element).is('.active')){
+      $(list[position+1]).trigger('click');
+    }
+  })
+});
+
+
+$('.jPaginateBack').on('click', function(){
+  var list = $('.pagination').find('li');
+  $.each(list, function(position, element){
+    if($(element).is('.active')){
+      $(list[position-1]).trigger('click');
+    }
+  })
+});
